ec41a6c1058b61f4a1b89b0f2badd4e18b0a050d
hw24a054-web <hw24a054@oecu.jp>
2026-09-07 16:12:01 +0900

    LP更新

---
+++ script.js
@@ -0,0 +1,7 @@
+function openContact() {
+    document.getElementById("contactModal").style.display = "flex";
+}
+
+function closeContact() {
+    document.getElementById("contactModal").style.display = "none";
+}
