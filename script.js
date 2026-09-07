3a58b78e3933effa24a8d26362739c84c3574830
hw24a054-web <hw24a054@oecu.jp>
2026-09-07 20:33:31 +0900

    LP更新

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function openContact() {
 function closeContact() {
     document.getElementById("contactModal").style.display = "none";
 }
+
+// メニューを開く
+function openMenu() {
+    document.getElementById("menuPanel").classList.add("open");
+}
+
+// メニューを閉じる
+function closeMenu() {
+    document.getElementById("menuPanel").classList.remove("open");
+}
